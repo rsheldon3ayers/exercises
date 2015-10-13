7e14954d26c62c5c496789e996957454735a8f71
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2015-10-13 13:50:21 -0500

    updates

--- grades/grades.js
+++ grades/grades.js
@@ -1,4 +1,7 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var currentScore,
+    highestScore = 0,
+    lowestScore = 101;
 var arrayF = [];
 var arrayD = [];
 var arrayC = [];
@@ -6,32 +9,42 @@ var arrayB = [];
 var arrayA = [];
 
 for (var i = 0; i < scores.length; i++) {
+	currentScore = scores[i];
+	if (currentScore > highestScore) {
+		highestScore = currentScore;
+	}
+	if (currentScore < lowestScore) {
+		lowestScore = currentScore;
+	}
 
-	if (scores[i] < 60) {
-    arrayF.push(scores[i]);
-	console.log("Grade is F");
 
-	} else if (scores[i] <= 70) {
-		
-		arrayD.push(scores[i]);
-		console.log("Grade is D");
-	} else if (scores[i] <= 80) {
-		arrayC.push(scores[i]);
-		console.log("Grade is C")
-	} else if (scores[i] <= 90) {
-		arrayB.push(scores[i]);
-		console.log("Grade is B");
-	}  else if (scores[i] <= 100) {
-		arrayA.push(scores[i]);
-		console.log("Grade is A");
-	} 
+
+
+
+switch (true) {
+
+	case (currentScore < 61):
+		arrayF.push(currentScore);
+		break;
+	case (currentScore < 71):
+		arrayD.push(currentScore);	
+		break;
+	case (currentScore < 81):
+		arrayC.push(currentScore);	
+		break;
+	case (currentScore < 91):
+		arrayB.push(currentScore);	
+		break;
+	case (currentScore < 101):
+		arrayA.push(currentScore);	
+		break;	
+	}
 }
-var highestGrade = Math.max.apply(Math, scores);
-console.log("Highest Grade is ", highestGrade);
-var lowestGrade = Math.min.apply(Math, scores);
-console.log("Lowest Grade is ", lowestGrade);
+console.log("Highest Score", highestScore);
+console.log("Lowest Score", lowestScore);
 console.log("Total F's:", arrayF.length);
 console.log("Total D's:", arrayD.length);
 console.log("Total C's:", arrayC.length);
 console.log("Total B's:", arrayB.length);
 console.log("Total A's:", arrayA.length);
+
